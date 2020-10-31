1a4ff6632001de263ef1886c5c27145225fd8f8a
Rahul-Karunakaran <67697032+Rahul-Karunakaran@users.noreply.github.com>
2020-10-31 10:00:13 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,27 @@
+class Paper{
+    constructor(x,y){
+        var option={
+            'isStatic':false,
+            'restitution':0.1,
+            'friction':0.5,
+            'density':1.2
+        }
+        this.body = Bodies.circle(x,y,5,option)
+        this.radius = 5;
+        this.width = 5 ;
+        this.height = 5;
+        this.paper = loadImage("./paper.png")
+        World.add(world, this.body);
+        
+
+    }
+    display(){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(this.body.angle);
+        imageMode(CENTER);
+        image(this.paper, 0, 0, this.width, this.height);
+        pop();
+      }
+   
+}
